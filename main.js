9756a2a71fdb9299703059cab0aa6079d56b0afb
Callam Farrell <calfazzo@hotmail.com>
2019-04-23 16:16:56 -0400

    made the text appear in the centre of the screen!

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// The $ sign is JQuery, that is a javascript library, it helps you do things that would be harder with pure javascript
 $(".button").click(function() {
     $("#optional-text").toggle();
   });
